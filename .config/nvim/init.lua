aac641fecfd74c00e698ab814b5d5e5c061bf44c
namnh198 <me@namhoainguyen.com>
2025-07-27 01:27:40 +0700

    feat(nvim): debug neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,22 @@
+if vim.loader then
+  vim.loader.enable()
+end
+
+_G.dd = function(...)
+  require("snacks.debug").inspect(...)
+end
+
+_G.bt = function()
+  require("snacks.debug").backtrace()
+end
+
+_G.p = function(...)
+  require("snacks.debug").profile(...)
+end
+
+vim.print = _G.dd
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require("config.lazy").load({
+  -- debug = true
+})
