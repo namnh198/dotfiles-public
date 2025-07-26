3ae0a1cb83241cf3cf083c77fe582da19dfcad98
namnh198 <me@namhoainguyen.com>
2025-07-27 01:17:19 +0700

    fix(nvim): rounded lualine

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -67,17 +67,15 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
-      local LazyVim = require("lazyvim.util")
-      opts.sections.lualine_c[4] = {
-        LazyVim.lualine.pretty_path({
-          length = 0,
-          relative = "cwd",
-          modified_hl = "MatchParen",
-          directory_hl = "",
-          filename_hl = "Bold",
-          modified_sign = "",
-          readonly_icon = " 󰌾 ",
-        }),
+      opts.options.section_separators = { left = "", right = "" }
+      opts.options.component_separators = { left = "", right = "" }
+      opts.sections.lualine_a = {
+        {
+          "mode",
+          fmt = function(str)
+            return " " .. str
+          end,
+        },
       }
     end,
   },
