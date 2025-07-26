5225453f61666792d8cc25f26db653817ce879f7
namnh198 <me@namhoainguyen.com>
2025-07-27 01:15:10 +0700

    fix(nvim): make sure all keymap silent by default

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,28 +6,28 @@ local map = vim.keymap.set
 
 -- General
 map({ "n", "i", "s", "x" }, "<C-a>", "gg<S-v>G", { desc = "selected all" })
-map("x", "<leader>d", "y'o<Esc>p", { desc = "duplicate selected", noremap = true, silent = true })
+map("x", "<leader>d", "y'o<Esc>p", { desc = "duplicate selected" })
 
 -- Buffers
 map("n", "<tab>", "<cmd>bnext<cr>", { desc = "Prev buffer" })
 map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Next buffer" })
 
 -- Split windows
-map("n", "ss", ":split<Return>", { desc = "split horizontal", noremap = true, silent = true })
-map("n", "sv", ":vsplit<Return>", { desc = "split vertical", noremap = true, silent = true })
+map("n", "ss", ":split<Return>", { desc = "split horizontal" })
+map("n", "sv", ":vsplit<Return>", { desc = "split vertical" })
 
 -- Copying file path
 map("n", "<leader>cf", function()
   local path = vim.fn.expand("%")
   vim.fn.setreg("+", path)
   vim.notify("Copied relative path:\n" .. path)
-end, { desc = "copy relative path", noremap = true, silent = true })
+end, { desc = "copy relative path" })
 
 map("n", "<leader>cF", function()
   local path = vim.fn.expand("%:p")
   vim.fn.setreg("+", path)
   vim.notify("Copied absolute path:\n" .. path)
-end, { desc = "copy absolute path", remap = true, silent = true })
+end, { desc = "copy absolute path", remap = true })
 
 -- Formating
 map({ "n", "v" }, "<leader>fm", function()
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,6 +6,15 @@ local o = vim.opt
 
 vim.g.ai_cmp = false
 
+-- Make sure all keymaps silent by default
+local map = vim.keymap.set
+---@diagnostic disable-next-line: duplicate-set-field
+vim.keymap.set = function(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  return map(mode, lhs, rhs, opts)
+end
+
 -- Swap
 o.swapfile = false
 
