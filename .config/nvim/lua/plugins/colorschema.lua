fe8026632af3cc140bc9eb10c1fee3ba1ce4a051
namnh198 <me@namhoainguyen.com>
2025-09-06 15:34:01 +0700

    fix(nvim): fix catppuccin theme for bufferline

--- .config/nvim/lua/plugins/colorschema.lua
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -1,13 +1,24 @@
 return {
   {
     "catppuccin/nvim",
+    lazy = true,
     name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-      transparent_background = true,
-    },
+    opts = function(_, opts)
+      opts.flavor = "mocha"
+      opts.transparent_background = true
+      opts.no_italic = true
+      opts.float = {
+        transparent = true,
+        solid = false,
+      }
+      -- Issue: https://github.com/LazyVim/LazyVim/issues/6355
+      local module = require("catppuccin.groups.integrations.bufferline")
+      if module then
+        module.get = module.get_theme
+      end
+      return opts
+    end,
   },
-
   {
     "LazyVim/LazyVim",
     opts = {
