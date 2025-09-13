cce93006c51b750f4b915606f4c82797281726b5
namnh198 <me@namhoainguyen.com>
2025-09-14 01:05:19 +0700

    fix(nvim): show hidden files snacks.nvim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -100,7 +100,11 @@ return {
     opts = function(_, opts)
       local sources = opts.picker.sources or {}
       local source_names = { "files", "explorer", "grep", "grep_word", "grep_buffers" }
-      local source_opts = { hidden = true, ignored = true, exclude = { ".git", ".DS_Store", ".vscode", ".idea" } }
+      local source_opts = {
+        hidden = true,
+        ignored = true,
+        exclude = { ".git", ".DS_Store", ".vscode", ".idea", "node_modules", ".next", "vendor" },
+      }
 
       for _, name in ipairs(source_names) do
         sources[name] = source_opts
